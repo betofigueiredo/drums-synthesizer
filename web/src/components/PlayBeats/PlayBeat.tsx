974ba0a5f1689e8b2d866c5fe379565f739aef1c
Beto Figueiredo <beto@2sidesof1.com>
2023-09-03 19:54:33 -0300

    [web] feat: space between 4 4 blocks

--- web/src/components/PlayBeats/PlayBeat.tsx
+++ web/src/components/PlayBeats/PlayBeat.tsx
@@ -4,11 +4,17 @@ const PlayBeat = ({ stepNumber }: { stepNumber: number }) => {
   const stepLocation = useAppSelector((state) => state.machine.stepLocation);
   const isActive = stepLocation === stepNumber;
 
-  const className = isActive
-    ? "m-1.5 h-2 w-12 rounded-full bg-cyan-400 shadow-[0_0_6px_3px_rgb(34,211,238,40%)]"
-    : "m-1.5 h-2 w-12 rounded-full bg-black shadow-[inset_0_1px_1px_0_rgb(255,255,255,7%)]";
+  function getClassName() {
+    let className = isActive
+      ? "m-1.5 h-2 w-12 rounded-full bg-cyan-400 shadow-[0_0_6px_3px_rgb(34,211,238,40%)]"
+      : "m-1.5 h-2 w-12 rounded-full bg-black shadow-[inset_0_1px_1px_0_rgb(255,255,255,7%)]";
+    if (stepNumber % 4 === 0) {
+      className += " mr-7";
+    }
+    return className;
+  }
 
-  return <button type="button" className={className} />;
+  return <button type="button" className={getClassName()} />;
 };
 
 export default PlayBeat;
