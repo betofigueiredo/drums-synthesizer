6351082a97162d1d5720797f8e2b25a612698afd
Beto Figueiredo <beto@2sidesof1.com>
2024-01-09 21:17:06 -0300

    [feat] updates

--- api/use_cases/songs/get_song_use_case/test_get_song_use_case.py
+++ api/use_cases/songs/get_song_use_case/test_get_song_use_case.py
@@ -74,6 +74,32 @@ class TestCreateSongUseCase:
         assert result[0].get("message") == "Song not found."  # type: ignore
 
     # TEST
+    def test_kit_not_found(self):
+        def find_song_by_id(song_id):
+            return helpers.CreateDotDict(
+                {
+                    "id": "f551219f-da27-4d6d-9d31-907a015a5b45",
+                    "user_id": "2a253332-f9d5-4924-9c80-7856ee71e852",
+                }
+            )
+
+        def find_kit_by_id(kit_id):
+            return None
+
+        repository = RepositoryMock()
+        repository.songs.find_by_id = find_song_by_id  # type: ignore
+        repository.kits.find_by_id = find_kit_by_id  # type: ignore
+        utils = Utils()
+        result = get_song_use_case(
+            user_id="2a253332-f9d5-4924-9c80-7856ee71e852",
+            song_id="f551219f-da27-4d6d-9d31-907a015a5b45",
+            utils=utils,
+            repository=repository,  # type: ignore
+        )
+        assert result[0].get("code") == "KIT_NOT_FOUND"  # type: ignore
+        assert result[0].get("message") == "Kit not found for this song."  # type: ignore
+
+    # TEST
     def test_success(self):
         def find_song_by_id(song_id):
             return helpers.CreateDotDict(
@@ -83,15 +109,17 @@ class TestCreateSongUseCase:
                 }
             )
 
+        def find_kit_by_id(kit_id):
+            return helpers.CreateDotDict({"id": "636800dd-54b8-4284-8904-854fe4f01966"})
+
         repository = RepositoryMock()
         repository.songs.find_by_id = find_song_by_id  # type: ignore
+        repository.kits.find_by_id = find_kit_by_id  # type: ignore
         utils = Utils()
-
         result = get_song_use_case(
             user_id="2a253332-f9d5-4924-9c80-7856ee71e852",
             song_id="f551219f-da27-4d6d-9d31-907a015a5b45",
             utils=utils,
             repository=repository,  # type: ignore
         )
-
         assert result.get("song") is not None  # type: ignore
